c1ab322681474f9ae7825fe05432e431acb1d444
张良 <646565439@qq.com>
2018-12-27 19:50:30 +0800

    解决了一个bug: 发现界面，攻略，训练营和资讯三个页面滑动不可切换

--- bingqiu-ios/Find(发现)/Camp(训练营)/Controller/CampViewController.h
+++ bingqiu-ios/Find(发现)/Camp(训练营)/Controller/CampViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CampViewController : BaseViewController
+@interface CampViewController : UIViewController
 
 @end
 
--- bingqiu-ios/Find(发现)/Strategy(攻略)/Controller/StrategyViewController.h
+++ bingqiu-ios/Find(发现)/Strategy(攻略)/Controller/StrategyViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 NS_ASSUME_NONNULL_BEGIN
 
-@interface StrategyViewController : BaseViewController
+@interface StrategyViewController : UIViewController
 
 @end
 
